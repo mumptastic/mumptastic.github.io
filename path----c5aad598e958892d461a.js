b86e4071b8314b01ff1e3e7a0277a1f9503fd8a1
Michael Nichols <mumptastic@gmail.com>
2018-06-03 17:17:48 -0500

    new site

---
+++ path----c5aad598e958892d461a.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Mumptastic"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----c5aad598e958892d461a.js.map
